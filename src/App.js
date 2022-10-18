677bfd35fe9ebfe03e89f7968ecdc1ab41b5a844
khuslen <khuslen@nest-32s-iMac.local>
2022-10-18 19:30:21 +0800

    to do search

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useRef, useEffect } from "react";
+import './App.css';
 
 
 function App() {
-  const inputElemen = useRef()
-  const inputElement = useRef()
-
-  function submit() {
-    alert(`${inputElemen.current.value}, ${inputElement.current.value} `);
-    
-
-  }
+  const [list, setList] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [asdvf, akjsbdnlcdvsnjbh] = useState('')
 
   return (
-    <div className='container'>
-      <div className='sign'>Sign up  <div className='line'></div> </div>
-      <div className='Name'>Name</div>
-      <input type='email' ref={inputElemen} className='input' />
-      <div className='Name'>Email</div>
-      <input type='email' ref={inputElement} className='input' />
-      <div className='mini'> <button onClick={submit} className='button'>Submit</button></div>
+    <div id='line'>
+      <div id='title'> To do list</div>
+      <div id='container'>
+        <input className='input' value={inputValue} placeholder='input' onChange={(e) => setInputValue(e.target.value)} />
+        <Button className="button1" onClick={() => { setList([...list, inputValue]); setInputValue('') }} value="Add" />
+      </div>
+      <div className='tasks'>
+        {list.map((todo, index) => asdvf.length > 0 ? todo.startsWith(asdvf) &&
+          < Text value={todo} index={index} list={list} setList={setList} />
+          : <Text value={todo} index={index} list={list} setList={setList} />)}
+      </div>
+      <div>
+        <input onChange={(asdsad) => akjsbdnlcdvsnjbh(asdsad.target.value)} /> <button onClick={filt} />
+      </div>
     </div>
-  )
+
+  );
+  function filt() {
+
+  }
+  function Text({ value, index, list, setList }) {
+    const deleteTodo = () => {
+      const newlist = list.filter((cur) => cur !== value);
+      setList(newlist)
+    }
+    return (
+      <div className='task-container'>
+        <input className='checkbox' type={'checkbox'} />
+        <p>{`Task # ${index + 1} ${value}`}</p>
+
+        <Button className="button1" onClick={deleteTodo}
+          value="Delete" />
+      </div>
+    )
+  }
+  function Button(props) {
+    return <button className={props.className} onClick=
+      {props.onClick}>{props.value} </button>
+  }
 }
 
 
 export default App;
-
-
